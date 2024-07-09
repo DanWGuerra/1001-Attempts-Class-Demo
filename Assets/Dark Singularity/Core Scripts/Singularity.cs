722ba0361da9b6df9d896eb3631bdc0ce31ce451
Dan <danwg01@gmail.com>
2024-07-09 17:26:28 -0600

    Dark hole

---
+++ Assets/Dark Singularity/Core Scripts/Singularity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+//[RequireComponent(typeof(SphereCollider))]
+public class Singularity : MonoBehaviour
+{
+    //This is the main script which pulls the objects nearby
+    [SerializeField] public float GRAVITY_PULL = 100f;
+    public static float m_GravityRadius = 1f;
+
+    void Awake() {
+        m_GravityRadius = GetComponent<CircleCollider2D>().radius;
+
+        if(GetComponent<CircleCollider2D>()){
+            GetComponent<CircleCollider2D>().isTrigger = true;
+        }
+    }
+    
+    //void OnTriggerStay (Collider other) {
+    //    if(other.attachedRigidbody && other.GetComponent<SingularityPullable>()) {
+    //        float gravityIntensity = Vector3.Distance(transform.position, other.transform.position) / m_GravityRadius;
+    //        other.attachedRigidbody.AddForce((transform.position - other.transform.position) * gravityIntensity * other.attachedRigidbody.mass * GRAVITY_PULL * Time.smoothDeltaTime);
+    //    }
+    //}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.attachedRigidbody && collision.GetComponent<SingularityPullable>())
+        {
+            float gravityIntensity = Vector3.Distance(transform.position, collision.transform.position) / m_GravityRadius;
+            collision.attachedRigidbody.AddForce((transform.position - collision.transform.position) * gravityIntensity * collision.attachedRigidbody.mass * GRAVITY_PULL * Time.smoothDeltaTime);
+        }
+    }
+}
---
+++ Assets/Dark Singularity/Core Scripts/SingularityCore.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+//[RequireComponent(typeof(SphereCollider))]
+public class SingularityCore : MonoBehaviour
+{
+    //This script is responsible for what happens when the pullable objects reach the core
+    //by default, the game objects are simply turned off
+
+
+    //void OnTriggerStay (Collider other) {
+    //    if(other.GetComponent<SingularityPullable>()){
+    //        other.gameObject.SetActive(false);
+    //    }
+    //}
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<SingularityPullable>())
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    void Awake(){
+        if(GetComponent<CircleCollider2D>()){
+            GetComponent<CircleCollider2D>().isTrigger = true;
+        }
+    }
+}
